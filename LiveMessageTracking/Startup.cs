28069f71076d1910106116fe7f292df39b81c3ea
Neil Watson <drneilwatson@hotmail.com>
2017-03-13 15:12:28 -0400

    missed files during initial upload

---
+++ LiveMessageTracking/Startup.cs
@@ -0,0 +1,26 @@
+﻿
+using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR;
+[assembly: OwinStartup(typeof(LiveMessageTracking.Startup))]
+namespace LiveMessageTracking
+{
+    public class Startup
+    {
+        /// <summary>
+        /// Configure the OWIN pipeline to host singlarR
+        /// </summary>
+        /// <param name="app"></param>
+        public void Configuration(IAppBuilder app)
+        {
+            //connection string to the SQL Server database used for backplaning signalR
+            string sqlConnectionString = "Server=testserver;Database=LiveMessageTracking;Integrated Security=True;";
+
+            //use SQL server as the backplane for signalR
+            GlobalHost.DependencyResolver.UseSqlServer(sqlConnectionString);
+            
+            //map the signalR pipeline, e.g http://localhost:8081/signalr
+            app.MapSignalR();
+        }
+    }
+}
