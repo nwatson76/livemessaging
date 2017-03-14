3fc8acf224f6e8c9308a6ee963183e2121e987bd
Neil Watson <drneilwatson@hotmail.com>
2017-03-13 21:41:55 -0400

    update comments

--- LiveMessageTracking/Hubs/CommunicationHub.cs
+++ LiveMessageTracking/Hubs/CommunicationHub.cs
@@ -6,11 +6,15 @@ using Microsoft.AspNet.SignalR;
 
 namespace LiveMessageTracking.Hubs
 {
+
+    /// <summary>
+    /// Hub for Live Messaging app
+    /// </summary>
     public class CommunicationHub : Hub
     {
         public void Send(string dateTime, string serviceName, string entrySource, string statusMessage, string extraDetail, bool isError)
         {
-            // Call the addNewMessageToPage method to update clients.
+            // Call the addNewMessageToPage method to broadcast to all clients
             Clients.All.addNewMessageToPage(dateTime, serviceName, entrySource, statusMessage, extraDetail, isError);
         }
     }
